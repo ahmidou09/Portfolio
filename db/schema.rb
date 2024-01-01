f4b2f82055a6ba22d83e118b9dcfbd3b1721cb33
Ahmidou EL mehdi <ahmidouem@gmail.com>
2024-01-01 18:05:48 +0100

    Added a `user_id` column to the projects table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_28_175343) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_29_174838) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_28_175343) do
     t.string "live_link"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,6 +35,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_28_175343) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role"
   end
 
+  add_foreign_key "projects", "users"
 end
