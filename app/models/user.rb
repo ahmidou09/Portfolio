128979a4520e30ac07fdf1b78308fc8f1ec2b82d
Ahmidou EL mehdi <ahmidouem@gmail.com>
2024-01-01 18:03:58 +0100

    Created an `admin?` method for role checking.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
   has_secure_password
 
+  has_many :projects, dependent: :destroy
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true
+
+  enum role: { user: 0, admin: 1 }
+
+  def admin?
+    role == 'admin'
+  end
 end
