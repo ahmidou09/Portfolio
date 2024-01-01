2957d52f44ab76baaab134805441bc0328169181
Ahmidou EL mehdi <ahmidouem@gmail.com>
2024-01-01 18:04:11 +0100

    Added a new route for the `logout` action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :projects
-    resource :users, only: [:create]
-    post '/login', to: 'authentication#login'
+  resources :users, only: [:index, :show, :create, :update, :destroy]
+  post '/login', to: 'authentication#login'
+  post '/logout', to: 'authentication#logout'
 end
