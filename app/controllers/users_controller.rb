876fb2ee7fad14a39d203efc0aebcd513e0ab094
Ahmidou EL mehdi <ahmidouem@gmail.com>
2024-01-01 18:02:41 +0100

    Updated `user_params` to permit `role`

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+
     if @user.save
       render json: @user, status: 201
     else
@@ -33,7 +34,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.permit(:username, :password)
+    params.permit(:username, :password, :role)
   end
 
   def find_user
